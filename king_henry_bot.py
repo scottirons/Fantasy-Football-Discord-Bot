4a8208d6074ff19700be73c1b79423f561f7fed4
Scott Irons <scottpirons@gmail.com>
2021-09-06 15:51:12 -0400

    again

--- king_henry_bot.py
+++ king_henry_bot.py
@@ -31,7 +31,7 @@ async def on_message(message):
         await message.channel.send(text)
     if('trade') in message.content:
         await message.channel.send(file=discord.File('no trades.jpg'))
-    if (('who') or ('what')) and (('qb') or ('quarberback')) in message.content:
+    if (('who') or ('what')) and (('qb') or ('quarterback')) in message.content:
         await message.channel.send(random_qb.which_qb())
 
     
