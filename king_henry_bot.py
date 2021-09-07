bfa4cebe324991e92ce3feef7b878003b8485127
Scott Irons <scottpirons@gmail.com>
2021-09-07 15:25:31 -0400

    pls

--- king_henry_bot.py
+++ king_henry_bot.py
@@ -55,9 +55,9 @@ async def on_message(message):
         await message.channel.send(file=discord.File('watchyourprofanity.gif'))
     if ('Who should I start?') in message.content:
         await message.channel.send("what's your name?")
-            name = input()
+        name = input()
         await message.channel.send("what position")
-            position = input()
+        position = input()
         await message.channel.send(who_start(name, position)
 
 
