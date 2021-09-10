0c17a688bc0d774acf6bbb474adf627b8771dee9
Scott Irons <scottpirons@gmail.com>
2021-09-09 21:48:28 -0400

    8ball

--- king_henry_bot.py
+++ king_henry_bot.py
@@ -24,7 +24,6 @@ answers = ['yes', 'no', 'of course', 'perhaps', 'fuck if I know', "it's unlikely
            'sleep on it', 'it shall be done', 'my sources say no', 'nah', 'definitely',\
            'most likely', 'probs', "I'm not confident in it", 'shabalabanono', 'get J riggity riggity'\
            \'riggity riggity riggity rekt']
-]
 
 
 client = commands.Bot(command_prefix=['!'])
