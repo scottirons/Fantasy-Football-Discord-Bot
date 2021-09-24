dc24813b97695265493e8795a5a58f8db7dfb912
Scott Irons <scottpirons@gmail.com>
2021-09-24 17:03:35 -0400

    adding doubtful and fixing IR issue

--- injured_player.py
+++ injured_player.py
@@ -2,7 +2,7 @@ from injuries import injured_dict
 
 def injury(person):
     nested_dict = injured_dict()  # create the dictionary i think
-#    print(nested_dict)
+    print(nested_dict['Scott'][0])
     person = person.title()
     if person not in nested_dict:
         return('name does not exist')
@@ -10,7 +10,12 @@ def injury(person):
 
         injured = []
         questionable = []
+        doubtful = []
         counter = 1
+        try:
+            doubtful += (nested_dict[person][0]['DOUBTFUL'])
+        except:
+            counter += 1
         try:
             questionable += (nested_dict[person][0]['QUESTIONABLE'])
         except:
@@ -20,28 +25,50 @@ def injury(person):
         except:
             counter += 1
         try:
-            injured += (nested_dict[person][0]['IR'])
+            injured += (nested_dict[person][0]['INJURY_RESERVE'])
         except:
             counter += 13
 
-        if injured == [] and questionable == []:
+        if injured == [] and questionable == [] and doubtful == []: # H
             thing = (['Wow, your whole team is healthy!'])
             thing = '\n'.join(thing)
             return(thing)
         elif injured == []:
-            thing = (['The following players are questionable:'] + questionable)
-            thing = '\n'.join(thing)
-            return(thing)
+            if doubtful == []:
+                thing = (['The following players are questionable:'] + questionable) # F
+                thing = '\n'.join(thing)
+                return(thing)
+            elif questionable == []:
+                thing = (['The following players are doubtful:'] + doubtful) # G
+                thing = '\n'.join(thing)
+                return (thing)
+            else:
+                thing = (['The following players are questionable:'] + questionable + # E
+                         ['and the following players are doubtful:'] + doubtful)
+                thing = '\n'.join(thing)
+                return(thing)
         elif questionable == []:
-            thing = (['The following players are injured:'] + injured)
-            thing = '\n'.join(thing)
+            if doubtful == []:
+                thing = (['The following players are injured:'] + injured) # D
+                thing = '\n'.join(thing)
+                return(thing)
+            else:
+                thing = (['The following players are doubtful:'] + doubtful + # C
+                ['and the following players are injured:'] + injured)
+                thing ='\n'.join(thing)
+                return(thing)
+        elif doubtful == []:
+            thing = (['The following players are questionable:'] + questionable + # B
+                     ['and the following players are injured:'] + injured)
+            thing = '\n'.format(thing)
             return(thing)
         else:
-            thing = (['The following players are questionable:'] + questionable +
-                   ['and the following players are injured:'] + injured)
+            thing = (['The following players are questionable:'] + questionable + # A
+                   ['the following players are injured:'] + injured +
+                    ['and the following players are doubtful:'] + doubtful)
             thing = '\n'.join(thing)
             return(thing)
 
        #     return(nested_dict[person][0]['OUT'])
 
-print(injury('scott'))
+# print(injury('liam'))
