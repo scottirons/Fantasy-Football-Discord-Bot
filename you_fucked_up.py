057f477aa21b7b60be6d7b836fc529119508108f
Scott Irons <87613441+scottirons@users.noreply.github.com>
2022-09-28 08:05:25 -0400

    fixed goober index in the case where a "not again" player needs to be used in flex

--- you_fucked_up.py
+++ you_fucked_up.py
@@ -1,4 +1,6 @@
 from espn_api.football import League
+from collections import defaultdict
+import heapq
 
 
 league = League(league_id=41302936, year=2022, espn_s2='AEBZs%2F0JhLRPJvsLxD28BaBMEXt4wQELeh'\
@@ -42,61 +44,34 @@ def get_gameday_roster(team, week):
             current_points = box_scores[i].home_score
     return current_roster, current_points
 
-# def replace_value(dictionary,position,location, name, week):
-#     team = get_team(name)
-#     roster = get_gameday_roster(team, week)[0]
-#     for player in roster:
-#         if player.position == position:
-#             if player.points > dictionary[location]:
-#                 dictionary[location] = player.points
 
 def max_points(name, week):
     team = get_team(name)
     roster = get_gameday_roster(team, week)[0]
     points = round(get_gameday_roster(team, week)[1], 2)
     max_score_dict = {'QB': 0, 'RB1': 0, 'RB2': 0, 'WR1': 0, 'WR2': 0, 'FLEX': 0, 'TE': 0, 'K': 0, 'DEF': 0}
-    not_again = set()
+    positions = defaultdict(list)
     for player in roster:
-        if player.position == 'QB':
-            if player.points > max_score_dict['QB']:
-                max_score_dict['QB'] = player.points
-        elif player.position == 'K':
-            if player.points > max_score_dict['K']:
-                max_score_dict['K'] = player.points
-        elif player.position == 'D/ST':
-            if player.points > max_score_dict['DEF']:
-                max_score_dict['DEF'] = player.points
-        elif player.position == 'TE':
-            if player.points > max_score_dict['TE']:
-                max_score_dict['TE'] = player.points
-                not_again.add(player.name)
-        elif player.position == 'RB':
-            if player.points > max_score_dict['RB1']:
-                max_score_dict['RB1'] = player.points
-                not_again.add(player.name)
-        elif player.position == 'WR':
-            if player.points > max_score_dict['WR1']:
-                max_score_dict['WR1'] = player.points
-                not_again.add(player.name)
+        positions[player.position].append(-player.points)
 
-    for player in roster:
-        if player.name in not_again:
-            continue
-        elif player.position == 'RB':
-            if player.points > max_score_dict['RB2']:
-                max_score_dict['RB2'] = player.points
-                not_again.add(player.name)
-        if player.position == 'WR':
-            if player.name not in not_again:
-                if player.points > max_score_dict['WR2']:
-                    max_score_dict['WR2'] = player.points
-                    not_again.add(player.name)
+    for position in positions:
+        heapq.heapify(positions[position])
 
-    for player in roster:
-        if player.position in ['WR', 'TE', 'RB']:
-            if player.name not in not_again:
-                if player.points > max_score_dict['FLEX']:
-                    max_score_dict['FLEX'] = player.points
+    max_score_dict['QB'] = -heapq.heappop(positions['QB'])
+    max_score_dict['RB1'] = -heapq.heappop(positions['RB'])
+    max_score_dict['RB2'] = -heapq.heappop(positions['RB'])
+    max_score_dict['WR1'] = -heapq.heappop(positions['WR'])
+    max_score_dict['WR2'] = -heapq.heappop(positions['WR'])
+    max_score_dict['TE'] = -heapq.heappop(positions['TE'])
+    max_score_dict['K'] = -heapq.heappop(positions['K'])
+    max_score_dict['DEF'] = -heapq.heappop(positions['D/ST'])
+    if positions['WR']:
+        max_score_dict['FLEX'] = max(max_score_dict['FLEX'], -heapq.heappop(positions['WR']))
+    if positions['RB']:
+        max_score_dict['FLEX'] = max(max_score_dict['FLEX'], -heapq.heappop(positions['RB']))
+    if positions['TE']:
+        max_score_dict['FLEX'] = max(max_score_dict['FLEX'], -heapq.heappop(positions['TE']))
+
     max_score = round((sum(max_score_dict.values())), 2)
     difference = round((max_score - points), 2)
     return "In week " + str(week) + " you scored " + str(points) + " points.\n"\
@@ -105,4 +80,4 @@ def max_points(name, week):
 
 
 if __name__ == "__main__":
-    print(max_points('ethan', 1))
+    print(max_points('jon', 3))
